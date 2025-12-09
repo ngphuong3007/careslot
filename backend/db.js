b2cb2a5c360ba39b7e0306dfb2b3d96255e4da25
phuongynguye <nyphuong2611@gmail.com>
2025-12-10 00:27:48 +0700

    init

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-  host: 'localhost',      // Địa chỉ server MySQL (của XAMPP)
-  user: 'root',           // User mặc định của XAMPP
-  password: '',           // Password mặc định của XAMPP là rỗng
-  database: 'careslot_nha_khoa', // Tên database bạn sẽ tạo cho nha khoa
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
